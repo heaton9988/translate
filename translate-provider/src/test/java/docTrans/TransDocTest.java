5d2d2c9697657dd19604a53dcf0692cbf9705099
周志俊 <zhijun.zhou@utflc.com>
2022-05-26 13:13:11 +0800

    批量翻译优化

--- translate-provider/src/test/java/docTrans/TransDocTest.java
+++ translate-provider/src/test/java/docTrans/TransDocTest.java
@@ -1,3 +1,6 @@
+package docTrans;
+
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
@@ -6,43 +9,38 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.junit.Test;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-public class WordRead {
+public class TransDocTest {
     private static String fileDir = "/Users/xingchuan/Desktop/";
-    private static String filePathPrefix = fileDir + "Blue Blood True Blood";
+    private static String filePathPrefix = fileDir + "The White Owl Legends";
     private static String filePathOld = filePathPrefix + ".docx";
     private static String filePathNew = filePathPrefix + "_" + System.currentTimeMillis() + ".docx";
     private static String filePathCacheBaiduTxt = filePathPrefix + "_baidu.txt";
 
     private static String txtSeperator = "==========";
+    private static String txtSeperatorRegex = "={5,}";
 
     private static Gson gson = new Gson();
 
-    public static void main(String[] args) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8610; i++) {
-            sb.append("a");
-        }
-        System.out.println(sb.length());
-        System.out.println(new Date() + "\t" + sb.toString());
-        String zh = TransUtil.getZhByEn(sb.toString());
-        System.out.println(new Date() + "\t" + zh);
-    }
-
-    public static void translateWord() throws Exception {
+    @Test
+    public void translateWord() throws Exception {
         Map<String, String> en2zh = collectTransTxt();
         generateWordByTxt(en2zh);
     }
 
     private static Map<String, String> readTransTxt(String path) throws Exception {
         Map<String, String> retMap = Maps.newLinkedHashMap();
-        List<String> lines = Files.readLines(new File(path), StandardCharsets.UTF_8);
+        File txtFile = new File(path);
+        if (!txtFile.exists()) {
+            txtFile.createNewFile();
+        }
+        List<String> lines = Files.readLines(txtFile, StandardCharsets.UTF_8);
         for (String line : lines) {
             if (StringUtils.isBlank(line)) continue;
             String[] ss = line.split(txtSeperator);
@@ -55,32 +53,69 @@ public class WordRead {
     private static Map<String, String> collectTransTxt() throws Exception {
         Map<String, String> en2zh = readTransTxt(filePathCacheBaiduTxt);
 
-        FileWriter fw = new FileWriter(filePathCacheBaiduTxt, true);
-        BufferedWriter bw = new BufferedWriter(fw);
         XWPFDocument document = new XWPFDocument(new FileInputStream(filePathOld));
 
-        List<XWPFParagraph> paras = document.getParagraphs();
-        for (XWPFParagraph para : paras) {
+        List<String> textList = Lists.newArrayList();
+        for (XWPFParagraph para : document.getParagraphs()) {
             String text = para.getText();
             text = text.replaceAll("\n", "").replaceAll("\r", "");
 
             if (StringUtils.isBlank(text)) continue;
+            if (en2zh.containsKey(text)) continue;
 
-            if (en2zh.containsKey(text)) {
-                continue;
-            }
-
-            String zh = TransUtil.getZhByEn(text);
-            String enSeperateZh = text + txtSeperator + zh;
-
-            bw.newLine();
-            bw.write(enSeperateZh);
-            bw.flush();
-
-            en2zh.put(text, zh);
+            textList.add(text);
         }
-        bw.close();
 
+        FileWriter fw = new FileWriter(filePathCacheBaiduTxt, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        int succ = 0, fail = 0, ex = 0;
+        StringBuilder batchTexts = new StringBuilder();
+        for (int i = 0; i < textList.size(); i++) {
+            String text = textList.get(i);
+            try {
+                batchTexts.append(text).append(txtSeperator);
+
+                if (batchTexts.length() > 500 || i == textList.size() - 1) {
+                    String strBatchTexts = batchTexts.toString();
+                    batchTexts.setLength(0);
+                    String batchChineses = TransUtil.getZhByEn(strBatchTexts);
+                    String[] ssChinese = batchChineses.split(txtSeperatorRegex);
+                    String[] ssEnglish = strBatchTexts.split(txtSeperatorRegex);
+                    if (ssChinese.length != ssEnglish.length) {
+                        fail++;
+                        for (String en : ssEnglish) {
+                            String zh = TransUtil.getZhByEn(en);
+
+                            String enSeperateZh = en + txtSeperator + zh;
+
+                            bw.newLine();
+                            bw.write(enSeperateZh);
+                            bw.flush();
+
+                            en2zh.put(en, zh);
+                        }
+                    } else {
+                        succ++;
+                        for (int j = 0; j < ssEnglish.length; j++) {
+                            bw.newLine();
+                            bw.write(ssEnglish[j] + txtSeperator + ssChinese[j]);
+                            bw.flush();
+
+                            en2zh.put(ssEnglish[j], ssChinese[j]);
+                        }
+                    }
+                    System.out.println("ex=" + ex + "\tsucc=" + succ + "\tfail=" + fail);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                ex++;
+            }
+        }
+        System.out.println("ex=" + ex + "\tsucc=" + succ + "\tfail=" + fail);
+
+
+        bw.close();
         return en2zh;
     }
 
